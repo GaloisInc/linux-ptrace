e75b2d927297769c2eacfa5bed9f7f0679678e24
Joe Hendrix <jhendrix@galois.com>
2016-12-27 12:17:34 -0800

    Add support for GHC 8.0.1

--- System/Linux/Ptrace/GenStruct.hs
+++ System/Linux/Ptrace/GenStruct.hs
@@ -1,17 +1,42 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-
+Module           : Reopt.Semantics.Implementation
+Copyright        : (c) Galois, Inc 2015-2016
 
+This defines a template haskell function for creating storable records.
+-}
+{-# OPTIONS_GHC -Werror #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
 module System.Linux.Ptrace.GenStruct where
 
 import Control.Monad
 import Foreign
 import Language.Haskell.TH
 
+-- | This creates a storable structure containing a list of fields each with the same type.
 genStruct :: String -> [String] -> Q Type -> Q [Dec]
 genStruct name ctors elemType = do
   let name' = mkName name
   elemType' <- elemType
+#if MIN_VERSION_template_haskell(2,11,0)
+  let strictness = Bang NoSourceUnpackedness NoSourceStrictness
+  let varsAndTypes = (\nm -> (mkName nm, strictness, elemType')) <$> ctors
+  let typeDecl :: Dec
+      typeDecl = DataD [{-context-}]
+                       name'
+                       [{-tyvars-}]
+                       Nothing
+                       [RecC name' varsAndTypes]
+                       [ConT ''Show]
+#else
   varsAndTypes <- mapM (\n -> varStrictType (mkName n) (strictType notStrict elemType)) ctors
-  let typeDecl = DataD [{-context-}] name' [{-tyvars-}] [RecC name' varsAndTypes] [{-deriving-}''Show]
+  let typeDecl :: Dec
+      typeDecl = DataD [{-context-}]
+                       name'
+                       [{-tyvars-}]
+                       [RecC name' varsAndTypes]
+                       [''Show]
+#endif
   -- Could evaluate this now, but what happens if we're cross-compiling? Is CInt the target's size, or ours?
   let elemSize = [|sizeOf (undefined :: $(elemType))|]
 
@@ -35,7 +60,7 @@ genStruct name ctors elemType = do
 
   -- Eek, can't substitute this below: TH lifting is not referentially transparent
   let numCtors = length ctors
-  
+
   storableInst : [] <- [d| instance Storable $(return $ ConT name') where
                               sizeOf _ = numCtors * $(elemSize)
                               alignment _ = alignment (undefined :: $(elemType))
--- System/Linux/Ptrace/X86Regs.hs
+++ System/Linux/Ptrace/X86Regs.hs
@@ -8,6 +8,7 @@ import System.Linux.Ptrace.GenStruct
 
 -- see user_regs_struct in <sys/user.h>
 
+x86_regs :: [String]
 x86_regs = ["ebx","ecx","edx","esi","edi","ebp","eax","xds","xes","xfs","xgs","orig_eax","eip","xcs","eflags","esp","xss"]
--- Can't reuse x86_regs here. Damn those stage restrictions.
+
 genStruct "X86Regs" ["ebx","ecx","edx","esi","edi","ebp","eax","xds","xes","xfs","xgs","orig_eax","eip","xcs","eflags","esp","xss"] [t|Word32|]
